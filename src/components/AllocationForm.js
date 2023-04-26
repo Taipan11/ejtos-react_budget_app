6006a4b67b6006be169981eab3eae92eb8554552
Mehdi KHAMASSI <mehdikhamassi@MacBook-Pro-de-Mehdi.local>
2023-04-26 14:33:11 +0200

    termine

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -9,13 +9,11 @@ const AllocationForm = (props) => {
     const [action, setAction] = useState('');
 
     const submitEvent = () => {
-
             if(cost > remaining) {
                 alert("The value cannot exceed remaining funds  £"+remaining);
                 setCost("");
                 return;
             }
-
         const expense = {
             name: name,
             cost: parseInt(cost),
